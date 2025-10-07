4bfaa8e78b93edb873b1a798b6d297bbd87d89f0
yogesh4492 <yogeshpatel26042005@gmail.com>
2025-10-07 15:39:37 +0530

    modified:   1.txt 	new file:   Eda_da/1.py

---
+++ Eda_da/1.py
@@ -0,0 +1,10 @@
+""" Eda in Data Analysis:
+Exploratory Data Analysis 
+
+its used to eal with large amount of data and also with data sets 
+three types of Eda:
+1)univarite Analysis
+2)Bivarite Analysis
+3)Multivarite Analysis
+
+"""
